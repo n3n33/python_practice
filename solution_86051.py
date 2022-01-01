2221e5dd60c0e8145374d0a52d86cc468617bd55
Ye Na Kwon <yenakwon@Yeui-MacBookAir.local>
2022-01-01 22:18:38 +0900

    4

---
+++ solution_86051.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+def solution(numbers):
+    answer = -1
+    
+    num = 45
+    tmp = 0     
+    for item in range(len(numbers)):
+        tmp += numbers[item]
+    
+    answer = num - tmp
+    return answer
