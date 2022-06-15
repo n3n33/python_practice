688e54e25f90ab7d75c1bac9a6205895f60ef11c
whi0331 <whi0331@hanmail.net>
2022-06-15 14:51:54 +0900

    0615

--- baekjoon/boj_15651.py
+++ baekjoon/boj_15651.py
@@ -1,1 +1,21 @@
- dd
+import sys
+
+#n, m = 4, 3
+n,m = map(int, sys.stdin.readlines().split(' '))
+
+selected = [ 0 for _ in range(m) ] # 0이 저장된 배열
+used = [ 0 for _ in range(n+1) ] #0이 저장된 배열
+
+def rec_func(k):
+    if k == m:
+        for  x in selected:
+            sys.stdout.write(str(x) + ' ')
+        sys.stdout.write('\n')
+
+    else:
+        for candidate in range(1,n+1):
+            selected[k] = candidate
+            rec_func(k+1)
+            selected[k] = 0
+
+rec_func(0)
