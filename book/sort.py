e980a1ba9035bff47591ecc6e733977fa9af0b0c
n3n33 <whi0331@hanmail.net>
2022-07-10 23:33:08 +0900

    삽입정렬

--- book/sort.py
+++ book/sort.py
@@ -1,12 +1,24 @@
 # 선택 정렬 
-array = [7,5, 9, 0, 3, 1, 6, 2, 4, 8]
+array1 = [7,5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(array)):
+for i in range(len(array1)):
     min_index = i
-    for j in range(i+1, len(array)):
-        if array[min_index] > array[j]:
+    for j in range(i+1, len(array1)):
+        if array1[min_index] > array1[j]:
             min_index = j
-    array[i], array[min_index] = array[min_index], array[i] # 스와프
+    array1[i], array1[min_index] = array1[min_index], array1[i] # 스와프
 
 
-print(array)
+print(array1)
+
+# 삽입 정렬
+array2 = [7,5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array2)):
+    for j in range(i, 0, -1): # 인덱스 i분터 1까지 감소
+        if array2[j] < array2[j - 1] : # 왼쪽으로 이동
+            array2[j], array2[j-1] = array2[j-1], array2[j]
+        else:
+            break
+
+print(array2)
