6dc0ad3f88dff9d60da1f2efd5073dd8706c828e
whi0331 <whi0331@hanmail.net>
2022-07-11 14:40:18 +0900

    국영수

---
+++ baekjoon/boj_10825.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/10825
+
+n = int(input())
+students = [] # 학생 정보를 담을 리스트
+
+# 모든 학생 정보를 입력 받기
+for _ in range(n):
+    students.append(input().split())
+
+students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+# 정렬된 학생 정보에서 이름만 출력
+for student in students:
+    print(student[0])
