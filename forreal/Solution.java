9f920fd1255f57eeda51e2495815eb0612c762b4
whi0331 <whi0331@hanmail.net>
2022-07-18 11:03:06 +0900

    temp

--- forreal/Solution.java
+++ forreal/Solution.java
@@ -3,21 +3,9 @@ package forreal;
 class Solution {
     public int solution (int bridge_length, int weight, int[] worker_weights){
       int answer = 0;
-      int[] bridge = new int[bridge_length];
-      for (int i =0; i< bridge_length; i++){
-        bridge[i] = 0;
-      }
-      int sum_bridge = 0;
-      for (int i = 0; i <= bridge_length-1; i++){        
-        for (int j=0; j<= worker_weights.length-1; j++){      
-          bridge[i] = worker_weights[j];
-          sum_bridge += bridge[i];
-          if (sum_bridge > weight){
-            bridge[i] = worker_weights[j+1];
-            answer ++;
-          }
-        }
-      }
+      
+      
+
       return answer;
       
     }
